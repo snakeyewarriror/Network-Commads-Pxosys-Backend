2ee7ae4c9891bece5b087eb7ccada0e8b86daabc
snakeyewarrior <snakeyewarrior@email.com>
2025-05-25 22:19:39 +0100

    Changed settings.py

--- src/pxosys/settings.py
+++ src/pxosys/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-us#wvn01*01_me^36zh97xi1%*6d&=_d2s4o6^se=%9p3y^ijl'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
